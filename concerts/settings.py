d95b5fca3b99670e2a318eb46af329166cd12324
Allison Edwards <allisonedwards12@yahoo.com>
2021-10-31 17:31:14 -0500

    Added the login and register pages. Beginnings of the backend

--- concerts/settings.py
+++ concerts/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'landing.apps.LandingConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +126,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'landing-home'
+LOGIN_URL = 'login'
--- landing/views.py
+++ landing/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+post = [
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted:': 'August 28, 2018'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Landing</h1>')
+    context = {
+        'posts': post
+    }
+    return render(request, 'landing/home.html', context) 
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'landing/about.html', {'title':'About'}) 
