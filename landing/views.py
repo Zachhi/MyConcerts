b41bb6c39edeaf8dd098d081bd1a77f849c6ac84
Emory Fields <emory.c.fields@tamu.edu>
2021-11-06 11:18:03 -0500

    TicketMaster API included and concerts title and url to concert are listed on front end

--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+import requests
+'''
 post = [
     {
         'author': 'Jane Doe',
@@ -14,12 +15,22 @@ post = [
         'date_posted:': 'August 28, 2018'
     }
 ]
-
+'''
 def home(request):
+    response = requests.get('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=US&apikey=HCme8Zo9DSUpVKCGGF9CbgcTKO3YbsjE&page=1') 
+    # try changing p = 2!!!!!!!!!!!!! on line 12
+    # super easy pagination-type querying 
+    concerts = response.json()
+    concerts = concerts["_embedded"]
+    events = concerts["events"]
+    return render(request, "landing/home.html", {"events": events})
+
+    '''
     context = {
         'posts': post
     }
-    return render(request, 'landing/home.html', context) 
+    return render(request, 'landing/home.html', context)
+    ''' 
 
 def about(request):
     return render(request, 'landing/about.html', {'title':'About'}) 
