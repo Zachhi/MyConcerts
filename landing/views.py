0ca9ca308095921a6288f81e24e1b3045bfaf952
Emory Fields <emory.c.fields@tamu.edu>
2021-11-13 09:40:46 -0600

    added error handling for spotify authentication and worked on filtering concerts

--- landing/views.py
+++ landing/views.py
@@ -4,49 +4,32 @@ from landing.credentials import CLIENT_ID, CLIENT_SECRET
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from . import credentials
-'''
-post = [
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted:': 'August 28, 2018'
-    }
-]
+from datetime import date
 
-def home(request):
-    response = requests.get('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=US&apikey=HCme8Zo9DSUpVKCGGF9CbgcTKO3YbsjE&page=1') 
-    # try changing p = 2!!!!!!!!!!!!! on line 12
-    # super easy pagination-type querying 
-    concerts = response.json()
-    concerts = concerts["_embedded"]
-    events = concerts["events"]
-    return render(request, "landing/home.html", {"events": events})
-
-    context = {
-        'posts': post
-    }
-    return render(request, 'landing/home.html', context)
-'''
 
 def get_spotify_info(): #carefull will call authetentification each time its called.. so we really woudl like to only call once
     SCOPE = "user-library-read, user-top-read, user-follow-read, user-read-email, user-read-private, playlist-read-private"
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri = 'http://127.0.0.1:8000/', scope=SCOPE))
     topartists = sp.current_user_top_artists(limit=20, time_range='long_term')
     toptracks = sp.current_user_top_tracks(limit=20, time_range='long_term')
-    toptrack = toptracks['items'][0]['name']
-    #print(toptrack)
-    artist = topartists['items'][0]['genres']
+
+    try:
+        toptrack = toptracks['items'][0]['name'] #try catch block here in case the user does not have any data
+    except: 
+        print("User does not have any toptracks")
+        return()
+    
+    try:
+        topartist = topartists['items'][0]['genres']
+    except: 
+        print("User does not have any top artists")
+        return()    
+
     user_top = {}
     toptracks_name = []
     topartists_name = []
     topgenres_name = []
+    
     for i in range(20):
         toptracks_name.append(toptracks['items'][i]['name'])
     user_top["toptracks"] = toptracks_name
@@ -59,12 +42,15 @@ def get_spotify_info(): #carefull will call authetentification each time its cal
         topgenres_name.append(topartists['items'][i]['genres'])
     user_top["topgenres"] = topgenres_name
     return user_top
+    
 
-userlisttop = get_spotify_info()
+#userlisttop = get_spotify_info()
+
+get_spotify_info()
+
 
 def home(request):
-    print(userlisttop["topartists"])
-    response = requests.get('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=US&apikey=HCme8Zo9DSUpVKCGGF9CbgcTKO3YbsjE&page=1') 
+    #print(userlisttop["topartists"])
     # try changing p = 2!!!!!!!!!!!!! on line above
     # super easy pagination-type querying 
     events = ticket_master_request()
@@ -86,12 +72,19 @@ def spotify_auth(request):
     print(results['items']['artist'])
 
 
-def ticket_master_request(genre = 'Pop'):
-    url = 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=' + genre + '&countryCode=US&apikey=HCme8Zo9DSUpVKCGGF9CbgcTKO3YbsjE&page=1'
+def ticket_master_request(genre = 'Country', city = 'Austin', page = 1, start_date = date.today().strftime("%Y-%m-%d"), end_date = '2022-12-25'):
+    url = 'https://app.ticketmaster.com/discovery/v2/events.json?&countryCode=US&apikey=HCme8Zo9DSUpVKCGGF9CbgcTKO3YbsjE&page=' + str(page)
+    if(city != ''):
+        url = url + '&city=' + city
+    if(genre != ''):
+        url = url + '&classificationName=' + genre
+    if(start_date != ''):
+        url = url + '&startDateTime=' + start_date + 'T00:00:00Z'
+    if(end_date != ''):
+        url = url + '&endDateTime=' +  end_date + 'T00:00:00Z'
+
     response = requests.get(url) 
-    # try changing p = 2!!!!!!!!!!!!! on line above
-    # super easy pagination-type querying 
-    
+
     concerts = response.json()
     concerts = concerts["_embedded"]
     events_from_api = concerts["events"]
