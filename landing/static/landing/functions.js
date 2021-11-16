5888ffab5d94b73f4ea540fe9fbfa97c9ae83425
Allison Edwards <allisonedwards12@yahoo.com>
2021-11-16 11:14:04 -0600

    Star button toggle

--- landing/static/landing/functions.js
+++ landing/static/landing/functions.js
@@ -2,18 +2,18 @@ function changeColor() {
     // change the overall color
 
     // change the toggle button
+    
+    // document.getElementById("star-btn").classList.toggle("starred");
+    btn = document.getElementById("star-btn");
     console.log(btn.classList)
-    document.getElementById("star-btn").classList.toggle("starred");
-    const btn = document.getElementById("star-btn");
-
     // store what the new mode is
-    /*if (localStorage.getItem("star") == "true") {
+    if (localStorage.getItem("star") == "true") {
         localStorage.setItem("star", "false");
         btn.style.color = "gray";
-        console.log(btn.value);
+        console.log(btn.style.color);
     } else {
         localStorage.setItem("star", "true");
         btn.style.color = "yellow";
-        console.log(btn.value);
-    } */
+        console.log(btn.style.color);
+    }
 }
