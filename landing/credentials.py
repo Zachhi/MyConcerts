cadf1b5a2e9c5bed4b0f376c45fcb788e122c314
emmahaeussler <emmahaeussler@tamu.edu>
2021-11-17 18:49:52 -0600

    credentials test

--- landing/credentials.py
+++ landing/credentials.py
@@ -1,4 +1,6 @@
-CLIENT_ID = '1bcc6dc0466942b9b6b27d29af1b1871'
-CLIENT_SECRET = 'd9ebd8f530004e57803514c16524f9e5'
+#CLIENT_ID = '1bcc6dc0466942b9b6b27d29af1b1871'
+CLIENT_ID = 'e87659a410df45f0a4dd304ce6ddd63a'
+#CLIENT_SECRET = 'd9ebd8f530004e57803514c16524f9e5'
+CLIENT_SECRET = '6350c16adea44b709d3aa443af20eace'
 REDIRECT_URI = 'http://127.0.0.1:8000/1/'
 SCOPE = "user-library-read, user-top-read, user-follow-read, user-read-email, user-read-private, playlist-read-private"
